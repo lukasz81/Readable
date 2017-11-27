1fe04e595a8fb11f390125d8587ac9e4fbb56776
lukasz81 <info@designmasters.co.uk>
2017-11-27 22:56:33 +0000

    core(redux) add files git

---
+++ frontend/src/app/posts/action-types.js
@@ -0,0 +1,4 @@
+export const STORE_POSTS = 'STORE_POSTS';
+export const ADD_TO_POSTS = 'ADD_TO_POSTS';
+export const REMOVE_FROM_POSTS = 'REMOVE_FROM_POSTS';
+export const TOGGLE_SORT = 'TOGGLE_SORT';
---
+++ frontend/src/app/posts/post/action-types.js
@@ -0,0 +1,6 @@
+export const SAVE_POST = 'SAVE_POST';
+export const DELETE_COMMENT = 'DELETE_COMMENT';
+export const ADD_COMMENTS = 'ADD_COMMENTS';
+export const EDIT_POST = 'EDIT_POST';
+export const EDIT_COMMENT = 'EDIT_COMMENT';
+export const SAVE_COMMENT = 'SAVE_COMMENT';
